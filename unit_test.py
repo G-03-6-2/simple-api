8b882398691fb2dc0c035bdd6b4c47f47138657d
suwitchadathong <64015155@kmitl.ac.th>
2023-10-05 09:18:27 +0700

    1

--- unit_test.py
+++ unit_test.py
@@ -5,8 +5,8 @@ from app import app
 class AppTestCase(unittest.TestCase):
     def test_hello_with_string(self):
         # test hello
-        res = app.hello("Rathachai")
-        self.assertEqual(res, "Hello, Rathachai")
+        res = app.hello("Group3")
+        self.assertEqual(res, "Hello, Group3")
 
     def test_hello_with_number(self):
         # test hello
