9362fdfce87bd708c457da4e466a8145ae071638
64015142supamit <98441693+64015142supamit@users.noreply.github.com>
2023-10-11 16:29:43 +0700

    Update app.py

--- app/app.py
+++ app/app.py
@@ -28,5 +28,22 @@ def calculate(num1, num2):
 
     return jsonify(results)
 
+@app.route('/is_prime/<number>', methods=['GET'])
+def is_prime(number):
+    if number <= 1:
+        return False
+    if number <= 3:
+        return True
+    if number % 2 == 0 or number % 3 == 0:
+        return False
+
+    i = 5
+    while i * i <= number:
+        if number % i == 0 or number % (i + 2) == 0:
+            return False
+        i += 6
+
+    return True
+
 if __name__ == '__main__':
     app.run()
