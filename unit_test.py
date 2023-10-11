a8bcfcec54be5c2edee627cef5bf63a1e6667065
64015142supamit <98441693+64015142supamit@users.noreply.github.com>
2023-10-11 18:28:27 +0700

    Update unit_test.py

--- unit_test.py
+++ unit_test.py
@@ -5,7 +5,7 @@ from app import app
 class AppTestCase(unittest.TestCase):
     def test_hello_with_string(self):
         # test hello test
-        # docker rm -f $(docker ps -aq)
+        # docker rm -f $(docker ps -aq) 
         # docker rmi -f $(docker images -aq)
         res = app.hello("Group_3!")
         self.assertEqual(res, "hello, Group_3!")
